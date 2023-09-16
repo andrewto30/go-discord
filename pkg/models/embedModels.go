695a7f787255ad820ab42d6f90cbdc432fdf22c6
Andrew To <tui97308@temple.edu>
2023-09-16 19:49:08 -0400

    Added Thumbnail struct

--- pkg/models/embedModels.go
+++ pkg/models/embedModels.go
@@ -15,15 +15,15 @@ type EmbedComponents struct {
 	URL         string    `json:"url"`
 	Color       int       `json:"color"`
 	Footer      Footer    `json:"footer"`
-	Image       Image     `json:"image"`
-	Thumbnail   Thumbnail `json:"thumbnail"`
+	Image       Image     `json:"image,omitempty"`
+	Thumbnail   Thumbnail `json:"thumbnail,omitempty"`
 	Author      Author    `json:"author"`
 	Field       Field     `json:"fields"`
 }
 
 type Footer struct {
 	Text    string `json:"text"`
-	IconURL string `json:"icon_url"`
+	IconURL string `json:"icon_url,omitempty"`
 }
 
 type Image struct {
@@ -31,16 +31,17 @@ type Image struct {
 }
 
 type Thumbnail struct {
+	URL string `json:"url"`
 }
 
 type Author struct {
 	Name    string `json:"name"`
 	URL     string `json:"url"`
-	IconURL string `json:"icon_url"`
+	IconURL string `json:"icon_url,omitempty"`
 }
 
 type Field struct {
 	Name   string `json:"name"`
 	Value  string `json:"value"`
-	Inline bool   `json:"inline"`
+	Inline bool   `json:"inline,omitempty"`
 }
