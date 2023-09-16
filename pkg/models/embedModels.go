6e0bfdc5681226b05bbe20dd70c26f7cd21edf7c
Andrew To <tui97308@temple.edu>
2023-09-16 19:26:28 -0400

    Added Embed struct to represent Discord embed object

--- pkg/models/embedModels.go
+++ pkg/models/embedModels.go
@@ -1,1 +1,13 @@
 package models
+
+// Embed is a struct representing Discord embed object
+type Embed struct {
+	Username  string            `json:"username"`
+	AvatarURL string            `json:"avatar_url"`
+	Content   string            `json:"content"`
+	Embeds    []EmbedComponents `json:"embeds"`
+}
+
+// EmbedComponents is a struct representing Discord embed object components
+type EmbedComponents struct {
+}
